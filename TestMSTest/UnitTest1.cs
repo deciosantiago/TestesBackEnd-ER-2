ede9c43ef03780d0c7c08fb06b5d100b49084a38
deciosantiago <deciosantiago.ds@gmail.com>
2022-06-13 21:14:34 -0300

    first commit

---
+++ TestMSTest/UnitTest1.cs
@@ -0,0 +1,40 @@
+using ER2Teste;
+
+namespace TestMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SomarDoisNumeros()
+        {
+            //Arrange
+            double pNum = 1;
+            double sNum = 1;
+            double rNum = 2;
+
+            //Act
+            var resultado = Operacoes.Somar(pNum, sNum);
+
+            //Assert
+            Assert.AreEqual(rNum, resultado);
+
+        }
+
+
+        [DataTestMethod]
+        [DataRow(1, 1, 2)]
+        [DataRow(2, 1, 3)]
+        [DataRow(2, 2, 5)]
+
+        public void SomarDoisNumerosLista(double pNum, double sNum, double rNum)
+        {
+            //Act
+            var resultado = Operacoes.Somar(pNum, sNum);
+
+            //Assert
+            Assert.AreEqual(rNum, resultado);
+
+        }
+    }
+}
